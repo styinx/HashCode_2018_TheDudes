ffee53d746082849a852d63aa3c6aba2f59a5903
Samuel Beck <Sammy_13@gmx.net>
2018-03-01 20:02:21 +0100

    Added class ride.py

---
+++ Task/ride.py
@@ -0,0 +1,21 @@
+
+class Ride:
+
+    def __init__(self, x_start, y_start, x_end, y_end, earliest_start, latest_finish):
+        self.x_start = x_start
+        self.y_start = y_start
+        self.x_end = x_end
+        self.y_end = y_end
+        self.earliest_start = earliest_start
+        self.latest_finish = latest_finish
+        self.distance = self.calculate_distance(x_start, y_start, x_end, y_end)
+
+    def __init__(self, x_start, y_start):
+        self.x_start = x_start
+        self.y_start = y_start
+
+    def calculate_distance(self, x_start, y_start, x_end, y_end):
+        distance = abs(x_start - x_end) + abs(y_start - y_end)
+        return distance
+
+    
